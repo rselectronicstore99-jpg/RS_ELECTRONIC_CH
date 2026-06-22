5494d918b7b5e1e2681d988aee8b34d45fb1510e
rselectronicstore99-jpg <rselectronicstore99@gmail.com>
2026-06-23 00:13:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import string
 import gspread
 from datetime import datetime
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
